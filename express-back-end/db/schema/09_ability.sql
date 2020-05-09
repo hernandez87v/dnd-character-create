fc1fd9198af125ae47781c58f628514384e8168e
hernandez87v <hernandez87v@hotmail.com>
2020-05-08 19:25:42 -0700

    Add Ability file placeholders for table/seeds

---
+++ express-back-end/db/schema/09_ability.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS abilities CASCADE;
+CREATE TABLE abilities
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255)
+);
---
+++ express-back-end/db/seeds/09_abilities.sql
@@ -0,0 +1,3 @@
+INSERT INTO Abilities
+    (Name)
+VALUES
