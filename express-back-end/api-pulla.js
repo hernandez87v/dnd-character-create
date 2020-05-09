0e919e4d7ca68de1e9accd7551c18c988d062313
hernandez87v <hernandez87v@hotmail.com>
2020-05-09 09:46:56 -0700

    Added abilities seed data

---
+++ express-back-end/api-pulla.js
@@ -0,0 +1,34 @@
+const request = require('request');
+
+const apiComber = function(url) {
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    const allItems = data.results
+    for (i = 0; i < allItems.length; i++) {
+      // console.log(allItems[i].name)
+      let itemURL = `http://dnd5eapi.co${allItems[i].url}`;
+      // console.log('this is the itemurl', itemURL)
+      
+      //nested request to access info about each
+      request(itemURL, (error, response, body) => {
+        if (error) {
+          callback(error, null);
+        }
+        const data = JSON.parse(body);
+        // console.log("this is the category ", data.equipment_category)
+        const currentInsertion = `('${data.full_name}'),`;
+        console.log(currentInsertion);
+      });
+
+
+    }
+
+    // console.log(data.results)
+  });
+
+};
+
+apiComber("http://dnd5eapi.co/api/ability-scores")
