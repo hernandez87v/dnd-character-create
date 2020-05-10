1476a580d2b6bba7e6062cb0094f77ba3c5eafa8
hernandez87v <hernandez87v@hotmail.com>
2020-05-10 12:06:25 -0700

    Create Sign In page

--- react-front-end/src/pages/login.jsx
+++ react-front-end/src/pages/login.jsx
@@ -1,44 +1,46 @@
-import React from "react";
-import UserLIst from "../components/users/users.js"
+import React from 'react';
+import UserLIst from '../components/users/users.js';
+import SignIn from '../components/signin';
 const Login = () => {
-// class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       message: 'Click the button to load data!'
-//     }
-//   }
-//   fetchData = () => {
-//     axios.post('/api/user') // You can simply make your requests to "/api/whatever you want"
-//     .then((response) => {
-//       console.log('hi',response)
-//       // handle success
-//       //console.log(response.data) // The entire response from the Rails API
-//       //console.log(response.data.message) // Just the message
-//       this.setState({
-//         message: response.data.message
-//       });
-//     }) 
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>{ this.state.message }</h1>
-//         <button onClick={this.fetchData} >
-//           Fetch Data
-//         </button>        
-//       </div>
-//     );
-//   }
-// }
-// export default App;
-    return (
+  // class App extends Component {
+  //   constructor(props) {
+  //     super(props)
+  //     this.state = {
+  //       message: 'Click the button to load data!'
+  //     }
+  //   }
+  //   fetchData = () => {
+  //     axios.post('/api/user') // You can simply make your requests to "/api/whatever you want"
+  //     .then((response) => {
+  //       console.log('hi',response)
+  //       // handle success
+  //       //console.log(response.data) // The entire response from the Rails API
+  //       //console.log(response.data.message) // Just the message
+  //       this.setState({
+  //         message: response.data.message
+  //       });
+  //     })
+  //   }
+  //   render() {
+  //     return (
+  //       <div className="App">
+  //         <h1>{ this.state.message }</h1>
+  //         <button onClick={this.fetchData} >
+  //           Fetch Data
+  //         </button>
+  //       </div>
+  //     );
+  //   }
+  // }
+  // export default App;
+  return (
     <div className="App">
-        <ul>
-<UserLIst/>
-</ul>
+      <ul>
+        <SignIn />
+        <UserLIst />
+      </ul>
     </div>
-    )
-}
+  );
+};
 
 export default Login;
