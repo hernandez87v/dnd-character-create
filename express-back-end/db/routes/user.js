255a61ba023edfaa6c63241f404e603f58831b68
hernandez87v <hernandez87v@hotmail.com>
2020-05-10 10:48:50 -0700

    Add user post

--- express-back-end/db/routes/user.js
+++ express-back-end/db/routes/user.js
@@ -1,117 +1,83 @@
-  const express = require('express');
-  const router  = express.Router();
-  
-  module.exports = (db) => {
-    router.post("/", (req, res) => {
-      const name = req.body.name;
-      const nick_name = req.body.nick_name;
-      const email = req.body.email;
-      const password = req.body.password;
-      const values = [name, nick_name , email , password];
-      // const values = ['santi', 'santy76' ,'asn@asd.com' , 'password'];
+const express = require('express');
+const router = express.Router();
 
-      console.log('req',req.body)
-      
+module.exports = (db) => {
+  router.post('/', (req, res) => {
+    const name = req.body.name;
+    const nick_name = req.body.nick_name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const values = [name, nick_name, email, password];
+    // const values = ['santi', 'santy76' ,'asn@asd.com' , 'password'];
 
-      let query = `
+    console.log('req', req.body);
+
+    let query = `
       INSERT INTO users (name, nick_name , email , password)
       VALUES ($1, $2, $3, $4) RETURNING *;
       `;
-      db.query(query,values)
-        .then(data => {
-          const user = data.rows;
-          res.json({ user });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
+    db.query(query, values)
+      .then((data) => {
+        const user = data.rows;
+        res.json({ user });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-    // router.put("/appointments/:id", (request, response) => {
+  // router.put("/appointments/:id", (request, response) => {
 
-    //     const { student, interviewer } = request.body.interview;
-  
-    //   db.query(
-    //     `
-    //     INSERT INTO interviews (student, interviewer_id, appointment_id) VALUES ($1::text, $2::integer, $3::integer)
-    //     ON CONFLICT (appointment_id) DO
-    //     UPDATE SET student = $1::text, interviewer_id = $2::integer
-    //   `,
-    //     [student, interviewer, Number(request.params.id)]
-    //   )
-    //     .then(() => {
-    //       setTimeout(() => {
-    //         response.status(204).json({});
-    //         updateAppointment(Number(request.params.id), request.body.interview);
-    //       }, 1000);
-    //     })
-    //     .catch(error => console.log(error));
-    // });
-  
+  //     const { student, interviewer } = request.body.interview;
 
+  //   db.query(
+  //     `
+  //     INSERT INTO interviews (student, interviewer_id, appointment_id) VALUES ($1::text, $2::integer, $3::integer)
+  //     ON CONFLICT (appointment_id) DO
+  //     UPDATE SET student = $1::text, interviewer_id = $2::integer
+  //   `,
+  //     [student, interviewer, Number(request.params.id)]
+  //   )
+  //     .then(() => {
+  //       setTimeout(() => {
+  //         response.status(204).json({});
+  //         updateAppointment(Number(request.params.id), request.body.interview);
+  //       }, 1000);
+  //     })
+  //     .catch(error => console.log(error));
+  // });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-    router.get("/", (req, res) => {
-      let query = `
+  router.get('/', (req, res) => {
+    let query = `
       SELECT * FROM users;
       `;
-      db.query(query)
-        .then(data => {
-          console.log(data.rows)
-          const user = data.rows;
-          res.json({ user });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
-  
+    db.query(query)
+      .then((data) => {
+        console.log(data.rows);
+        const user = data.rows;
+        res.json({ user });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-
-    router.post("/:user_id", (req, res) => {
-      const values = [Number(req.params.poll_id)];
-      console.log(values);
-      let query = `
+  router.post('/:user_id', (req, res) => {
+    const values = [Number(req.params.poll_id)];
+    console.log(values);
+    let query = `
         UPDATE users
         SET completed = TRUE
         WHERE id = $1;
         `;
-      db.query(query, values)
-        .then(() => {
-          res.send('hi user');
-        })
-        .catch (err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
-  
-  
-    return router;
-  };
+    db.query(query, values)
+      .then(() => {
+        res.send('hi user');
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  return router;
+};
--- react-front-end/src/pages/newUser.jsx
+++ react-front-end/src/pages/newUser.jsx
@@ -1,44 +1,63 @@
-import React , { Component } from "react";
+import React, { Component } from 'react';
 import axios from 'axios';
-
 
 class newUser extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      message: 'Create a new user'
-    }
+      message: 'Create a new user',
+    };
   }
 
-
-  fetchData = () => { 
-      let name ='santy'
-      let nick_name = 'santy' 
-      let email = 'aasd' 
-      let password = 'asdasd'
-      
-      const user = {
-        name, nick_name , email, password
-      };
-
-    axios.post(`/api/user`, [user])
-      .then(res => {
-        console.log('hi',res);
+  fetchData = () => {
+    axios({
+      method: 'post',
+      url: '/api/user',
+      data: {
+        name: 'santi',
+        nick_name: 'santy87',
+        email: 'santi@santi.com',
+        password: 'asdasd',
+      },
+      validateStatus: (status) => {
+        return true;
+      },
+    })
+      .catch((error) => {
+        console.log(error);
       })
-  }
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  //   let name = 'santy';
+  //   let nick_name = 'santy';
+  //   let email = 'aasd';
+  //   let password = 'asdasd';
+
+  //   const user = {
+  //     name,
+  //     nick_name,
+  //     email,
+  //     password,
+  //   };
+
+  //   axios.post(`/api/user`, data: {user})
+  //   .then((res) => {
+  //     console.log('hi', res);
+  //   });
+  // };
   render() {
     return (
       <div className="newUser">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Create
-        </button>
+        <h1>{this.state.message}</h1>
+        <button onClick={this.fetchData}>Create</button>
       </div>
     );
   }
 }
 export default newUser;
-
 
 // const newUser = () => {
 //     return (
