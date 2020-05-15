46b84f9167b9149e81b7d4b54fae25f4e7e42525
hernandez87v <hernandez87v@hotmail.com>
2020-05-15 14:12:06 -0700

    Aligned appbar

--- react-front-end/src/components/navbar.jsx
+++ react-front-end/src/components/navbar.jsx
@@ -10,7 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import { spacing } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,32 +38,34 @@ const theme = createMuiTheme({
 
 export default function ButtonAppBar(props) {
   const classes = useStyles();
-  console.log('props',props)
-  if (props.login === true){
+  console.log('props', props);
+  if (props.login === true) {
     return (
       <div className={classes.root}>
         <MuiThemeProvider theme={theme}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="menu"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                DND testing
-              </Typography>
-              <Button href="/signup" color="inherit">
-                Sing Up
-              </Button>
-              <Button href="/login" color="inherit">
-                Login
-              </Button>
-            </Toolbar>
-          </AppBar>
+          <Box p={1} bgcolor="background.paper">
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  DND testing
+                </Typography>
+                <Button href="/signup" color="inherit">
+                  Sing Up
+                </Button>
+                <Button href="/login" color="inherit">
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </Box>
         </MuiThemeProvider>
       </div>
     );
@@ -71,43 +74,53 @@ export default function ButtonAppBar(props) {
       <div className={classes.root}>
         <MuiThemeProvider theme={theme}>
           <AppBar position="static">
-            <Toolbar>
-              <IconButton href = "/"
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="menu"
-              >
-                Home
-              </IconButton>
-              <IconButton href = "/createCharacter"
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="menu"
-              >
-                New Character
-
-              </IconButton>
-              <IconButton href = "/ShowCharacterByUser"
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="menu"
-              >
-                Show My Characters
-                
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                DND Character Creation
-              </Typography>
-              <Typography variant="h9" aligment = 'left' className={classes.title}>
-                santiFedericoni
-              </Typography>
-              <Button href="/" color="inherit">
-                LogOut
-              </Button>
-            </Toolbar>
+            <Box p={0}>
+              <Toolbar>
+                <IconButton
+                  size="small"
+                  href="/"
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  Home
+                </IconButton>
+                <IconButton
+                  size="small"
+                  href="/createCharacter"
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  New Character
+                </IconButton>
+                <IconButton
+                  size="small"
+                  href="/ShowCharacterByUser"
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  Show My Characters
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  <Box ml="18rem">DND Character Creation</Box>
+                </Typography>
+                <Typography
+                  variant="h9"
+                  aligment="left"
+                  className={classes.title}
+                >
+                  <Box ml="15rem">santiFedericoni</Box>
+                </Typography>
+                <Button href="/" color="inherit">
+                  LogOut
+                </Button>
+              </Toolbar>
+            </Box>
           </AppBar>
         </MuiThemeProvider>
       </div>
