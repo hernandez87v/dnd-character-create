f2289d027ad9b58b3d377c044fbb2ba8238412ef
hernandez87v <hernandez87v@hotmail.com>
2020-05-16 09:35:21 -0700

    Combat Container card complete

--- react-front-end/src/components/characters/overview/Combat_Container.js
+++ react-front-end/src/components/characters/overview/Combat_Container.js
@@ -8,6 +8,7 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 // ROUTE 3B - THIS IS AFTER CLICKING MAIN
 // QUICKVIEW > COMBAT CONTAINER
 const useStyles = makeStyles((theme) => ({
@@ -25,76 +26,126 @@ export default function CombatContainer() {
   const classes = useStyles();
 
   return (
-    <Box display="flex" alignItems="center" justifyContent="center">
-      <List>
-        <ListItem alignItems="center">
-          <ListItemAvatar>
-            <Avatar src="https://img.icons8.com/material/96/000000/sparring--v1.png" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Actions in Combat"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Attack, Cast a Spell, Dash, Disengage, Dodge, Grapple, Help,
-                  Hide, Improvise, Ready, Search, Shove, Use an Object
-                </Typography>
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="center">
-          <ListItemAvatar>
-            <Avatar
-              alt="Travis Howard"
-              src="https://img.icons8.com/material/96/000000/angry-fist.png"
+    <Container maxWidth="md">
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <List>
+          <ListItem alignItems="center">
+            <ListItemAvatar>
+              <Avatar src="https://img.icons8.com/material/96/000000/sparring--v1.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Actions in Combat"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    Attack, Cast a Spell, Dash, Disengage, Dodge, Grapple, Help,
+                    Hide, Improvise, Ready, Search, Shove, Use an Object
+                  </Typography>
+                  {' - Actions'}
+                </React.Fragment>
+              }
             />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Unarmed Strike"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  You can punch, kick, head-butt, or use a similar forceful blow
-                  and deal bludgeoning damage equal to 1 + STR modifier
-                </Typography>
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="center">
-          <ListItemAvatar>
-            <Avatar src="https://img.icons8.com/material/96/000000/toolbox--v1.png" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="TOOLS"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  className={classes.inline}
-                  color="textPrimary"
-                >
-                  Drum, Lyre, Shawm
-                </Typography>
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-      </List>
-    </Box>
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="center">
+            <ListItemAvatar>
+              <Avatar src="https://img.icons8.com/material/96/000000/angry-fist.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Unarmed Strike"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    You can punch, kick, head-butt, or use a similar forceful
+                    blow and deal bludgeoning damage equal to 1 + STR modifier
+                  </Typography>
+                  {' - Actions'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="center">
+            <ListItemAvatar>
+              <Avatar src="https://img.icons8.com/material/96/000000/toolbox--v1.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Actions in Combat"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    Two-Weapon Fighting
+                  </Typography>
+                  {' - Bonus Actions'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="center">
+            <ListItemAvatar>
+              <Avatar src="https://img.icons8.com/material/96/000000/wizard.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Spells"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    Misty Step(2nd)
+                  </Typography>
+                  {' - Bonus Actions'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="center">
+            <ListItemAvatar>
+              <Avatar src="https://img.icons8.com/material/96/000000/magical-scroll.png" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Bardic Inspiration"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    className={classes.inline}
+                    color="textPrimary"
+                  >
+                    As a bonus action, a creature (other than yourself) within
+                    60 ft. that can hear you gains an inspiration die (1d6). For
+                    10 minutes, the creature can add it to one ability check,
+                    attack roll, or saving throw. This can be added after seeing
+                    the roll, but before knowing the outcome.
+                  </Typography>
+                  {' - Bonus Actions'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
+    </Container>
   );
 }
