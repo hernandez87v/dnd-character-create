fc10e82c8dc588d5127d95d0a4d3f8470879100e
hernandez87v <hernandez87v@hotmail.com>
2020-05-16 09:06:56 -0700

    Add details to Combat Container

--- react-front-end/src/components/characters/overview/Combat_Container.js
+++ react-front-end/src/components/characters/overview/Combat_Container.js
@@ -1,93 +1,98 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams,
-  useRouteMatch,
-} from 'react-router-dom';
-import CombatStats from './Combat_Stats';
-import CombatWeapons from './Combat_Weapons';
-import CombatSpells from './Combat_Spells';
-import CombatDice from './Combat_Dice';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 // ROUTE 3B - THIS IS AFTER CLICKING MAIN
 // QUICKVIEW > COMBAT CONTAINER
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
 
-import AbilityPointsAll from './AP_all';
-// Since routes are regular React components, they
-// may be rendered anywhere in the app, including in
-// child elements.
-//
-// This helps when it's time to code-split your app
-// into multiple bundles because code-splitting a
-// React Router app is the same as code-splitting
-// any other React app.
-
-export default function CombatContainer(props) {
-  let { path, url } = useRouteMatch();
+export default function CombatContainer() {
+  const classes = useStyles();
 
   return (
-    <div>
-      <h2 className="view-title">Combat Container</h2>
-      <div className="overview-skill-throw">
-        <div className="skill-throw-internal">
-          <AbilityPointsAll
-            constitution={props.characterObject.constitution}
-            strength={props.characterObject.strength}
-            dexterity={props.characterObject.dexterity}
-            charisma={props.characterObject.charisma}
-            wisdom={props.characterObject.wisdom}
-            intelligence={props.characterObject.intelligence}
+    <Box display="flex" alignItems="center" justifyContent="center">
+      <List>
+        <ListItem alignItems="center">
+          <ListItemAvatar>
+            <Avatar src="https://img.icons8.com/material/96/000000/armored-breastplate.png" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="ARMOUR"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Light Armour
+                </Typography>
+              </React.Fragment>
+            }
           />
-        </div>
-      </div>
-      <ul className="character-nav">
-        <li>
-          <Link to={`${url}/stats`}>
-            <button variant="contained" color="primary">
-              Stats
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/weapons`}>
-            <button variant="contained" color="primary">
-              Weapons
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/spells`}>
-            <button variant="contained" color="primary">
-              Spells
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to={`${url}/dice`}>
-            <button variant="contained" color="primary">
-              Dice
-            </button>
-          </Link>
-        </li>
-      </ul>
-
-      <Switch>
-        <Route exact path={path}></Route>
-        <Route path={`${path}/stats`}>
-          <CombatStats characterObject={props.characterObject} />
-        </Route>
-        <Route path={`${path}/weapons`}>
-          <CombatWeapons characterObject={props.characterObject} />
-        </Route>
-        <Route path={`${path}/spells`}>
-          <CombatSpells characterObject={props.characterObject} />
-        </Route>
-        <Route path={`${path}/dice`}>
-          <CombatDice characterObject={props.characterObject} />
-        </Route>
-      </Switch>
-    </div>
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="center">
+          <ListItemAvatar>
+            <Avatar
+              alt="Travis Howard"
+              src="https://img.icons8.com/material/96/000000/sword.png"
+            />
+          </ListItemAvatar>
+          <ListItemText
+            primary="WEAPONS"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Crossbow, Hand, Longsword, Rapier, Shortsword, Simple Weapons
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="center">
+          <ListItemAvatar>
+            <Avatar src="https://img.icons8.com/material/96/000000/toolbox--v1.png" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="TOOLS"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Drum, Lyre, Shawm
+                </Typography>
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </List>
+    </Box>
   );
 }
