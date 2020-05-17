2b83aba7e8f8836a073ccae065c78652925ced07
santiago <santiago.federiconi@gmail.com>
2020-05-17 11:24:54 -0700

    making the show all character based on the user id

--- express-back-end/db/routes/character.js
+++ express-back-end/db/routes/character.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
+  router.get("/user/:id", (req, res) => {
+    console.log('all users' , req.params.id)
     let query = `
     SELECT characters.*, classes.name as character_class, races.name as race, backgrounds.name as background
     FROM characters 
     JOIN classes ON classes.id = class_id
     JOIN races ON races.id = race_id
-    JOIN backgrounds ON backgrounds.id = background_id;
+    JOIN backgrounds ON backgrounds.id = background_id
+    WHERE characters.user_id = ${req.params.id};
     `;
     db.query(query)
       .then(data => {
-        console.log(data.rows)
         const characters = data.rows;
         res.json({ characters });
       })
