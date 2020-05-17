1a0196d326b5f41890efb16d7ce6a94c8151395d
santiago <santiago.federiconi@gmail.com>
2020-05-16 18:49:02 -0700

    add conditional to login page

--- react-front-end/src/pages/login.jsx
+++ react-front-end/src/pages/login.jsx
@@ -2,12 +2,15 @@ import React, {  useState } from 'react';
 import SignIn from '../components/signin';
 
 const Login = (props) => {
-console.log('login.jsx page',props.loginState)
+  if (props.loginState.login !== true) {
   return (
     <div className="App">
       <SignIn login={props.loginState} setLogin = {props.setLogin}/>
     </div>
   );
+  } else {
+   return ( <p>hi!</p>)
+  }
 };
 
 export default Login;
