658d72a1ac275d35b5d067b5d0d15d3e007612c5
hernandez87v <hernandez87v@hotmail.com>
2020-05-17 11:36:35 -0700

    first commit on branch

--- express-back-end/db/routes/item.js
+++ express-back-end/db/routes/item.js
@@ -5,7 +5,7 @@ module.exports = (db) => {
   router.get('/', (req, res) => {
     console.log('ITEMS: ', req.params);
     let query = `
-    SELECT characters.*, items_owned.character_id as character_item
+    SELECT items.*, items_owned.character_id as character_item
     FROM characters
     JOIN items ON items.id = item_id
     JOIN items_owned ON items_owned.id = items_owned_id
@@ -25,3 +25,5 @@ module.exports = (db) => {
 // console.log('ITEMS: ', router);
 
 // make a query to the items_owned table where character id is equal to the current character, and you return all of the items_owned entries for that character_id
+
+//  JOIN items_owned ON items_owned.id = $1
