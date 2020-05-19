bab5fc069bc680a418e2893c687d555430e33555
hernandez87v <hernandez87v@hotmail.com>
2020-05-19 09:40:11 -0700

    Removed console log

--- react-front-end/src/components/characters/overview/Character_FeaturesProficiencies.js
+++ react-front-end/src/components/characters/overview/Character_FeaturesProficiencies.js
@@ -42,7 +42,6 @@ export default function CharacterFeaturesProficiencies(props) {
     };
     fetchData();
   }, []);
-  console.log('outside result: ', data);
 
   return (
     <Box display="flex" alignItems="center" justifyContent="center">
